52468843954fc32a434e91b126bfb28e29495ea0
redcommander561 <126519219+redcommander561@users.noreply.github.com>
2023-11-22 12:28:47 -0800

    we done

--- assignment.py
+++ assignment.py
@@ -9,62 +9,81 @@ def title():
     print("============")
     return None
 
-def instructions():
-    # Will display instructions
-    # input parameters: none needed
-    # output parameters: None
-    # Author:
-    # Modified:
-    return None
 
-import math
-def main():
-    calc = True
-    num = float(input(""))
-    while True:
-        math1 = (input(""))
-        num2 = float(input(""))
-        
+def help():
+    print("_________________________\n========\n= HELP =\n========\n+ = addition\n- = subtraction\nx = multiplication\n/ = division\n** = exponent\n_________________________\n")
+
+
+def instruct():
+    print("__________________________________________________\n================\n= INSTRUCTIONS =\n================\nStart by entering a number followed by +,-,/,x,**, then enter your second number.\nThe calculator will print the answer.\nYou may continue off your answer or type 'quit'.\n__________________________________________________\n")
     
-        if math1 == "+":
-            x = num + num2
-            num = x
-        if math1 == "x":
-            x = num * num2
-            num = x
-        if math1 == "**":
-            x = num **num2
-            num = x
-        if math1 == "-":
-            x = num - num2
-            num = x
-        if math1 == "/":
-            x = num / num2
-            num = x
-        print(f"= {x}")
-        
-
-
-
-        
-           
-        
-        
-            
-            
-            
-            
-
-        
-        
-            
-
-
-
-        title()
+def instructions():
     while True:
-        # keep giving options to choose menu options until they choose to exit
-        pass
+        menu = input('Type "instructions" "help" or "skip": ')
 
-if __name__ == "__main__":
-    main()
+
+        if menu != "instructions" and menu != "help" and menu != "skip":
+            print("invalid, try agian")
+        elif menu == "instructions":
+            instruct()
+            
+        elif menu == "help":
+            help()
+            
+        elif menu == "skip":
+            print("ok :)")
+            pass
+            return None
+
+
+
+
+        
+            
+
+
+title()
+instructions()
+num = float(input(""))
+while True:
+    math1 = (input(""))
+    num2 = float(input(""))
+    
+    
+    if math1 == "+":
+        x = num + num2
+        num = x
+    if math1 == "x":
+        x = num * num2
+        num = x
+    if math1 == "**":
+        x = num **num2
+        num = x
+    if math1 == "-":
+        x = num - num2
+        num = x
+    if math1 == "/":
+        x = num / num2
+        num = x
+    
+    print(f"= {x}")
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
